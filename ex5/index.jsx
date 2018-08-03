4505073f076388ee065513925e5612892cedbfc7
Joao Regis <joao.reegiis@gmail.com>
2018-08-02 23:00:28 -0300

    first commit

---
+++ ex5/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SilvaFamily from './silvafamily';
+
+ReactDOM.render(
+    <div>
+        <SilvaFamily />
+    </div>
+, document.getElementById('app'))
+
---
+++ utils/reactutils.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const childrenWithProps = (children, props) => (
+    React.Children.map(children, child => React.cloneElement(child, {...props}))
+)
+
+export { childrenWithProps }
